6efb0eebe51cc750917c33c0ec848cbf86d318b5
HU, Chenxi <chuak@connect.ust.hk>
2023-10-14 17:28:35 +0800

    add hubert

--- utils/dataset.py
+++ utils/dataset.py
@@ -3,7 +3,6 @@ import torch
 import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
-
 class MMSERDataset(Dataset):
     """multi model ser dataset."""
     
@@ -18,59 +17,41 @@ class MMSERDataset(Dataset):
             for sheet in sheets:
                 sheet_df = pd.concat([sheet_df, xl.parse(sheet)])
         self.df_ = sheet_df
+        
         # remove samples not agreed
-        self.df_["emotion_id"] = self.df_["emotion"].map(self.available_emotions)
-        self.df_ = self.df_[self.df_["emotion_id"].notna()].reset_index(drop=True)
         self.df_ = pd.merge(self.df_, self.df_text, on=["smp_id"])
+        self.df_["emotion_id"] = self.df_["emotion"].map(self.emo2id)
+        self.df_ = self.df_[self.df_["emotion_id"].notna()].reset_index(drop=True)
+        self.df_["session"] = self.df_["smp_id"].apply(lambda x: x.split("_")[0])
         
     def __load_text__(self, text_path):
         self.df_text = pd.read_csv(text_path)
         pass
     
-    def __build_features__(self, raw_list, audio_processer, batch_size=1000):
-        self.input_features = []
-        last_idx = 0
-        for idx in np.arange(batch_size, len(raw_list), batch_size):
-            print(idx)
-            self.input_features += audio_processer(raw_list[idx-batch_size:idx], 
-                                                        sampling_rate=AUDIORATE)['input_features']
-            last_idx = idx
-        self.input_features += audio_processer(raw_list[last_idx: len(raw_list)], 
-                                                    sampling_rate=AUDIORATE)['input_features']
-        self.input_features = torch.tensor(self.input_features)
-        
-    def __load_audio__(self, fn_path, audio_processer):
+    def __load_audio__(self, fn_path):
         self.fn_list = list(self.df_["smp_id"])
-        raw_list = []
+        self.raw_list = []
         for fn in self.fn_list:
-            raw_list.append(wavfile.read(os.path.join(fn_path, fn)+'.wav')[1])
-        self.__build_features__(raw_list, audio_processer)
+            self.raw_list.append(wavfile.read(os.path.join(fn_path, fn)+'.wav')[1])
     
     def __init__(self, 
                  fn_path, 
                  cutmap_path, 
                  text_path, 
-                 pretrained_model="openai/whisper-large"):
-        audio_processer = AutoProcessor.from_pretrained(pretrained_model)
-        self.available_emotions = {
-            "hap": 0,
-            "ang": 1,
-            "neu": 2,
-            "sad": 3,
-            "exc": 0,
-        }
+                 emo2id):
+        self.emo2id = emo2id
         self.__load_text__(text_path)
         self.__load_label__(cutmap_path)
-        self.__load_audio__(fn_path, audio_processer)
+        self.__load_audio__(fn_path)
         
     def __len__(self):
-        return self.input_features.shape[0]
+        return self.df_.shape[0]
     
     def __getitem__(self, idx):
         return {
-            "smp_id":  self.fn_list[idx],
-            "audio": self.input_features[idx],
-            "text": self.df_["transcript"][idx],
-            "emotion": self.df_["emotion"][idx],
-            "labels": self.df_["emotion_id"][idx]
+            "sess": list(self.df_["session"])[idx],
+            "fn": self.fn_list[idx],
+            "audio": self.raw_list[idx],
+            "text": list(self.df_["transcript"])[idx],
+            "labels": list(self.df_["emotion_id"])[idx]
         }
